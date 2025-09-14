6c4d98903b6904576cdaa710cf64c09a7c955f5d
rahul <meetmeerahul@gmail.com>
2025-09-14 23:12:20 +0530

    rollback to previous port

--- src/server.js
+++ src/server.js
@@ -56,5 +56,8 @@ initSocket(server);
 //   }
 // });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
